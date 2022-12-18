373d4ded1eb431404fcd07349bccf1dee58c961a
Humed-Muhammad <abc45171@gmail.com>
2022-12-18 23:57:35 +0300

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   await app.listen(5050);
 }
