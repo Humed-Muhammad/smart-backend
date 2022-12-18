0580bee082720408a4794ea53afbc74ecf71f779
Humed-Muhammad <abc45171@gmail.com>
2022-12-19 00:16:01 +0300

    Hotfix port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 5050;
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  await app.listen(5050);
+  await app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 }
 bootstrap();
