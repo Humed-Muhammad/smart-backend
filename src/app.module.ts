7241c95a87db7404b014b26306d9d96d2a1f1950
Humed-Muhammad <abc45171@gmail.com>
2023-04-13 20:46:12 +0300

    🗑️ Cleaning dead unused code and removing prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module, ValidationPipe } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
+
 import { APP_PIPE } from '@nestjs/core';
-import { AuthModule } from './auth/auth.module';
+
 import { ConfigModule } from '@nestjs/config';
 import { UploadsModule } from './uploads/uploads.module';
 
@@ -15,9 +14,7 @@ import { UploadsModule } from './uploads/uploads.module';
       driver: ApolloDriver,
       context: ({ req }) => ({ req }),
     }),
-    UsersModule,
-    PrismaModule,
-    AuthModule,
+
     UploadsModule,
     ConfigModule.forRoot({}),
   ],
